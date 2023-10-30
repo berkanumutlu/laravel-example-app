4dc903237b20e59b9b5558564f0c395a778f8d8a
Berkan Ümütlü <berkanxtr@gmail.com>
2023-10-30 18:59:18 +0300

    Web > Home sayfası oluşturuldu

---
+++ project/app/Http/Controllers/Web/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('web.home.index');
+    }
+}
--- project/routes/web.php
+++ project/routes/web.php
@@ -12,7 +12,4 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\Web\HomeController::class, "index"]);
