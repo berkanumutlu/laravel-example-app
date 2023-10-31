612b371b336ec008a00ed00e04e319087e1e9bd8
Berkan Ümütlü <berkanxtr@gmail.com>
2023-10-31 17:21:55 +0300

    Migration Article, Categories

---
+++ project/database/migrations/2023_10_31_120621_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 60);
+            $table->string("slug",150);
+            $table->string("description")->nullable();
+            $table->unsignedBigInteger("parent_id")->nullable();
+            $table->boolean("status")->default(0);
+            $table->boolean("feature_status")->default(0);
+            $table->integer("order")->default(0);
+            $table->string("seo_keywords")->nullable();
+            $table->string("seo_description")->nullable();
+            $table->timestamps();
+            $table->foreign("parent_id")->references("id")->on("categories")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
---
+++ project/database/migrations/2023_10_31_141654_create_article_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 80);
+            $table->string("slug", 160);
+            $table->text("body");
+            $table->string("image")->nullable();
+            $table->boolean("status")->default(0);
+            $table->string("tags")->nullable();
+            $table->string("seo_keywords")->nullable();
+            $table->string("seo_description")->nullable();
+            $table->integer("read_time")->default(0);
+            $table->integer("view_count")->default(0);
+            $table->integer("like_count")->default(0);
+            $table->dateTime("publish_date")->nullable();
+            $table->unsignedBigInteger("category_id");
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("category_id")->references("id")->on("categories")->onDelete("cascade");
+            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('article');
+    }
+};
