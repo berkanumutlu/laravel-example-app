6ae98f45d6cf42d0b70fc039f99f8c3c9544dff6
Berkan Ümütlü <berkanumutlu1@gmail.com>
2023-11-18 10:17:48 +0300

    Web > Anasayfada feature category list tasarımı yapıldı.

--- project/resources/views/web/home/index.blade.php
+++ project/resources/views/web/home/index.blade.php
@@ -3,10 +3,47 @@
 
 @endsection
 @section("style")
-
+    <link href="{{ asset('assets/web/css/home.min.css') }}" rel="stylesheet">
 @endsection
 @section("content")
-    
+    <div class="feature-category-list">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-xl-3">
+                    <div class="feature-category-item">
+                        <h2 class="title">Feature Category Title</h2>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+                            tincidunt odio a velit faucibus, in mattis quam laoreet.</p>
+                        <p class="footer-text">Lorem ipsum dolor sit amet</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-3">
+                    <div class="feature-category-item">
+                        <h2 class="title">Feature Category Title</h2>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+                            tincidunt odio a velit faucibus, in mattis quam laoreet.</p>
+                        <p class="footer-text">Lorem ipsum dolor sit amet</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-3">
+                    <div class="feature-category-item">
+                        <h2 class="title">Feature Category Title</h2>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+                            tincidunt odio a velit faucibus, in mattis quam laoreet.</p>
+                        <p class="footer-text">Lorem ipsum dolor sit amet</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-3">
+                    <div class="feature-category-item">
+                        <h2 class="title">Feature Category Title</h2>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+                            tincidunt odio a velit faucibus, in mattis quam laoreet.</p>
+                        <p class="footer-text">Lorem ipsum dolor sit amet</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @section("scripts")
 
