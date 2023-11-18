e1c58cce92ad73ad5ba551e7e09fca1721039c54
Berkan Ümütlü <berkanumutlu1@gmail.com>
2023-11-18 17:53:57 +0300

    Web > Popular article list, category list tasarımı yapıldı.

--- project/resources/views/web/home/index.blade.php
+++ project/resources/views/web/home/index.blade.php
@@ -4,6 +4,7 @@
 @endsection
 @section("style")
     <link href="{{ asset('assets/web/css/home.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/plugins/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 @endsection
 @section("content")
     <div class="feature-category-list">
@@ -44,7 +45,199 @@
             </div>
         </div>
     </div>
+    <main class="py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-9">
+                    <section class="popular-article-list">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="d-flex align-items-center justify-content-between">
+                                    <h2 class="font-weight-bold">Most Popular Articles</h2>
+                                    <div class="custom-pagination">
+                                        <span
+                                            class="custom-swiper-button-prev material-icons-outlined btn btn-secondary">arrow_back</span>
+                                        <span
+                                            class="custom-swiper-button-next material-icons-outlined btn btn-secondary">arrow_forward</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <!-- Slider main container -->
+                                <div class="swiper">
+                                    <!-- Additional required wrapper -->
+                                    <div class="swiper-wrapper">
+                                        <!-- Slides -->
+                                        <div class="swiper-slide">
+                                            <div class="popular-item">
+                                                <a href="#" class="d-block">
+                                                    <img src="https://via.placeholder.com/600x400" class="img-fluid"
+                                                         alt="Slider 1">
+                                                </a>
+                                                <div class="popular-item-body">
+                                                    <div class="author">
+                                                        Author: <a href="#">Berkan Ümütlü</a>
+                                                    </div>
+                                                    <div class="title">
+                                                        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                            Aliquam
+                                                            porta blandit risus ac consectetur.</h4>
+                                                    </div>
+                                                    <div class="date">
+                                                        <p>18 Oct 2023</p>
+                                                        <p class="ms-3">&#x25CF;10 min</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <div class="popular-item">
+                                                <a href="#" class="d-block">
+                                                    <img src="https://via.placeholder.com/600x400" class="img-fluid"
+                                                         alt="Slider 1">
+                                                </a>
+                                                <div class="popular-item-body">
+                                                    <div class="author">
+                                                        Author: <a href="#">Berkan Ümütlü</a>
+                                                    </div>
+                                                    <div class="title">
+                                                        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                            Aliquam
+                                                            porta blandit risus ac consectetur.</h4>
+                                                    </div>
+                                                    <div class="date">
+                                                        <p>18 Oct 2023</p>
+                                                        <p class="ms-3">&#x25CF;10 min</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <div class="popular-item">
+                                                <a href="#" class="d-block">
+                                                    <img src="https://via.placeholder.com/600x400" class="img-fluid"
+                                                         alt="Slider 1">
+                                                </a>
+                                                <div class="popular-item-body">
+                                                    <div class="author">
+                                                        Author: <a href="#">Berkan Ümütlü</a>
+                                                    </div>
+                                                    <div class="title">
+                                                        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                            Aliquam
+                                                            porta blandit risus ac consectetur.</h4>
+                                                    </div>
+                                                    <div class="date">
+                                                        <p>18 Oct 2023</p>
+                                                        <p class="ms-3">&#x25CF;10 min</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <div class="popular-item">
+                                                <a href="#" class="d-block">
+                                                    <img src="https://via.placeholder.com/600x400" class="img-fluid"
+                                                         alt="Slider 1">
+                                                </a>
+                                                <div class="popular-item-body">
+                                                    <div class="author">
+                                                        Author: <a href="#">Berkan Ümütlü</a>
+                                                    </div>
+                                                    <div class="title">
+                                                        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                            Aliquam
+                                                            porta blandit risus ac consectetur.</h4>
+                                                    </div>
+                                                    <div class="date">
+                                                        <p>18 Oct 2023</p>
+                                                        <p class="ms-3">&#x25CF;10 min</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-xl-3">
+                    <section class="sidebar sidebar-right category-list">
+                        <div class="title-section">
+                            <h4 class="title">Categories</h4>
+                        </div>
+                        <div class="content">
+                            <ul class="list-group list-unstyled">
+                                <li class="list-group-item"><a href="#" class="list-group-item-action">PHP
+                                        <span class="text-warning float-end me-3">&#x25CF;</span></a>
+                                </li>
+                                <li class="list-group-item"><a href="#" class="list-group-item-action">HTML
+                                        <span class="text-primary float-end me-3">&#x25CF;</span></a></li>
+                                <li class="list-group-item"><a href="#" class="list-group-item-action">CSS
+                                        <span class="text-danger float-end me-3">&#x25CF;</span></a></li>
+                                <li class="list-group-item"><a href="#" class="list-group-item-action">JS
+                                        <span class="text-success float-end me-3">&#x25CF;</span></a></li>
+                            </ul>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </main>
 @endsection
 @section("scripts")
-
+    <script src="{{ asset("assets/plugins/swiper/swiper-bundle.min.js") }}"></script>
+    <script>
+        $(document).ready(function () {
+            let swiper = new Swiper('.popular-article-list .swiper', {
+                speed: 400,
+                spaceBetween: 15,
+                slidesPerView: 3,
+                pagination: {
+                    el: '.swiper-pagination',
+                    type: 'bullets',
+                },
+                navigation: {
+                    nextEl: '.custom-swiper-button-next',
+                    prevEl: '.custom-swiper-button-prev'
+                },
+                /*autoplay: {
+                    delay: 5000,
+                },
+                rewind: true*/
+                breakpoints: {
+                    // when window width is >= 320px
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 10
+                    },
+                    480: {
+                        slidesPerView: 2,
+                        spaceBetween: 10
+                    },
+                    576: {
+                        slidesPerView: 2,
+                        spaceBetween: 10
+                    },
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 10
+                    },
+                    992: {
+                        slidesPerView: 3,
+                        spaceBetween: 15
+                    },
+                    1200: {
+                        slidesPerView: 3,
+                        spaceBetween: 15
+                    },
+                    1400: {
+                        slidesPerView: 3,
+                        spaceBetween: 15
+                    }
+                },
+            });
+        });
+    </script>
 @endsection
