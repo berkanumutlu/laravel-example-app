dcbdb3dc9603cd5a6a3cc48dac236eb97cbbfda3
Berkan Ümütlü <berkanumutlu1@gmail.com>
2023-11-23 21:13:18 +0300

    Web > Header search form eklendi.

--- project/app/Http/Controllers/Web/BaseController.php
+++ project/app/Http/Controllers/Web/BaseController.php
@@ -20,4 +20,9 @@ class BaseController extends Controller
         $this->data['favicon'] = asset('assets/web/images/logomark.min.svg');
         $this->data['site_name'] = config('app.name');
     }
+
+    public function search()
+    {
+        return view("web.home.index", $this->data);
+    }
 }
--- project/routes/web.php
+++ project/routes/web.php
@@ -13,3 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', [\App\Http\Controllers\Web\HomeController::class, "index"])->name('home');
+Route::get('/search', [\App\Http\Controllers\Web\BaseController::class, "search"])->name('search');
