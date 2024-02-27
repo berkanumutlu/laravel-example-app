19c134dd0a219e6b85ea0e70ec6d84f145dcb800
Berkan Ümütlü <berkanumutlu1@gmail.com>
2024-02-27 15:44:58 +0300

    Author component socials tasarımı değiştirildi. #2

--- project/resources/views/components/web/section-author.blade.php
+++ project/resources/views/components/web/section-author.blade.php
@@ -14,101 +14,77 @@
             <div class="description">{!! $authorDescription ?? '' !!}</div>
             <div class="socials">
                 <ul>
+                    <li class="website">
+                        <a aria-label="Learn more from my website" href="https://example.com" target="_blank"
+                           rel="nofollow"><i class="fa-solid fa-globe"></i></a>
+                    </li>
                     <li class="instagram">
-                        <a aria-label="Learn more from Instagram" href="https://instagram.com"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-instagram"></i>
-                        </a>
+                        <a aria-label="Learn more from Instagram" href="https://instagram.com" target="_blank"
+                           rel="nofollow"><i class="fa-brands fa-instagram"></i></a>
                     </li>
                     <li class="twitter">
-                        <a aria-label="Learn more from Twitter" href="https://twitter.com"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-twitter"></i>
-                        </a>
+                        <a aria-label="Learn more from Twitter" href="https://twitter.com" target="_blank"
+                           rel="nofollow"><i class="fa-brands fa-twitter"></i></a>
                     </li>
                     <li class="facebook">
-                        <a aria-label="Learn more from Facebook" href="https://facebook.com"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-facebook"></i>
-                        </a>
+                        <a aria-label="Learn more from Facebook" href="https://facebook.com" target="_blank"
+                           rel="nofollow"><i class="fa-brands fa-facebook"></i></a>
                     </li>
                     <li class="youtube">
-                        <a aria-label="Learn more from Youtube" href="https://youtube.com"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-youtube"></i>
-                        </a>
+                        <a aria-label="Learn more from Youtube" href="https://youtube.com" target="_blank"
+                           rel="nofollow"><i class="fa-brands fa-youtube"></i></a>
                     </li>
                     <li class="google">
-                        <a aria-label="Learn more from Google" href="https://google.com"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-google"></i>
-                        </a>
+                        <a aria-label="Learn more from Google" href="https://google.com" target="_blank" rel="nofollow">
+                            <i class="fa-brands fa-google"></i></a>
                     </li>
                     <li class="whatsapp">
-                        <a aria-label="Contact us on Whatsapp" href="https://whatsapp.com"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-whatsapp"></i>
-                        </a>
+                        <a aria-label="Contact us on Whatsapp" href="https://whatsapp.com" target="_blank"
+                           rel="nofollow"><i class="fa-brands fa-whatsapp"></i></a>
                     </li>
                     <li class="telegram">
-                        <a aria-label="Contact us on Telegram" href="https://telegram.org"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-telegram"></i>
-                        </a>
+                        <a aria-label="Contact us on Telegram" href="https://telegram.org" target="_blank"
+                           rel="nofollow"><i class="fa-brands fa-telegram"></i></a>
                     </li>
                     <li class="skype">
-                        <a aria-label="Contact us on Skype" href="https://skype.com"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-skype"></i>
-                        </a>
+                        <a aria-label="Contact us on Skype" href="https://skype.com" target="_blank" rel="nofollow">
+                            <i class="fa-brands fa-skype"></i></a>
                     </li>
                     <li class="linkedin">
                         <a aria-label="Learn more from LinkedIn" href="https://www.linkedin.com/in/berkanumutlu"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-linkedin"></i>
-                        </a>
+                           target="_blank" rel="nofollow"><i class="fa-brands fa-linkedin"></i></a>
                     </li>
                     <li class="github">
-                        <a aria-label="Learn more from Github" href="https://github.com/berkanumutlu"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-github"></i>
-                        </a>
+                        <a aria-label="Learn more from Github" href="https://github.com/berkanumutlu" target="_blank"
+                           rel="nofollow"><i class="fa-brands fa-github"></i></a>
                     </li>
                     <li class="medium">
-                        <a aria-label="Learn more from Medium" href="https://medium.com/@berkanumutlu"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-medium"></i>
-                        </a>
+                        <a aria-label="Learn more from Medium" href="https://medium.com/@berkanumutlu" target="_blank"
+                           rel="nofollow"><i class="fa-brands fa-medium"></i></a>
                     </li>
                     <li class="pinterest">
-                        <a aria-label="Learn more from Pinterest" href="https://pinterest.com"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-pinterest"></i>
-                        </a>
+                        <a aria-label="Learn more from Pinterest" href="https://pinterest.com" target="_blank"
+                           rel="nofollow"><i class="fa-brands fa-pinterest"></i></a>
                     </li>
                     <li class="tumblr">
-                        <a aria-label="Learn more from Tumblr" href="https://tumblr.com"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-tumblr"></i>
-                        </a>
+                        <a aria-label="Learn more from Tumblr" href="https://tumblr.com" target="_blank" rel="nofollow">
+                            <i class="fa-brands fa-tumblr"></i></a>
+                    </li>
+                    <li class="dribbble">
+                        <a aria-label="Learn more from Dribbble" href="https://dribbble.com/berkanumutlu"
+                           target="_blank" rel="nofollow"><i class="fa-brands fa-dribbble"></i></a>
                     </li>
                     <li class="wordpress">
-                        <a aria-label="Learn more from Wordpress" href="https://wordpress.com"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-wordpress"></i>
-                        </a>
+                        <a aria-label="Learn more from Wordpress" href="https://wordpress.com" target="_blank"
+                           rel="nofollow"><i class="fa-brands fa-wordpress"></i></a>
                     </li>
                     <li class="discord">
-                        <a aria-label="Join our Discord" href="https://discord.com"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-discord"></i>
-                        </a>
+                        <a aria-label="Join our Discord" href="https://discord.com" target="_blank" rel="nofollow">
+                            <i class="fa-brands fa-discord"></i></a>
                     </li>
                     <li class="paypal">
-                        <a aria-label="Donate on Paypal" href="https://paypal.com"
-                           target="_blank" rel="nofollow">
-                            <i class="fa-brands fa-paypal"></i>
-                        </a>
+                        <a aria-label="Donate on Paypal" href="https://paypal.com" target="_blank" rel="nofollow">
+                            <i class="fa-brands fa-paypal"></i></a>
                     </li>
                 </ul>
             </div>
